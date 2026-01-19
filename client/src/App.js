cdf4b073c74f4aaf9397373eead7643f2721fe6b
LydiaBrune <lydiasarabrune@gmail.com>
2026-01-19 21:01:25 +0530

    Initial Folder structure

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './assets/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to Banking App</h1>
+      </header>
+      <main>
+        <p>This is your banking application</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
